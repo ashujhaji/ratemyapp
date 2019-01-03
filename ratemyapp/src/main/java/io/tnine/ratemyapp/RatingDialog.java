9003e5178dc84371c62dd16ff79342896a87b8b5
Ashutosh Jha <ashujha.1996@gmail.com>
2019-01-03 15:56:47 +0530

    releasing for beta test

--- ratemyapp/src/main/java/io/tnine/ratemyapp/RatingDialog.java
+++ ratemyapp/src/main/java/io/tnine/ratemyapp/RatingDialog.java
@@ -1,17 +1,34 @@
 package io.tnine.ratemyapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Handler;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.RatingBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import io.tnine.ratemyapp.utils.Constants;
+import io.tnine.ratemyapp.utils.MyPref;
 
 public class RatingDialog {
 
     private static RatingDialog instance;
-    private Dialog dialog;
+    private static Dialog dialog;
     private TextView default_rating_msg, default_not_now;
+    private RatingBar ratingBar;
+    private static RelativeLayout rate_dialog_bg;
+
 
     public static RatingDialog getInstance(){
         if (instance == null){
@@ -20,14 +37,37 @@ public class RatingDialog {
         return instance;
     }
 
-    public void openRatingDialog(Context context, Activity activity, String your_app_name){
+    public static void initRateMyApp(Context context){
+        //------------------------shared preference init---------------------------
+        new MyPref.Builder().setContext(context)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(context.getPackageName())
+                .setUseDefaultSharedPreference(true)
+                .build();
+
+        if (!MyPref.getBoolean(Constants.IS_DIALOG_INITIALIZED,true)){
+            MyPref.putInt(Constants.COUNTS_FOR_DIALOG_OPEN,0);
+        }
+
+        //-------------------dialog init------------------------------
         dialog = new Dialog(context);
         dialog.setContentView(R.layout.rate_dialog);
+    }
 
+    public void openRatingDialog(final Context context, final Activity activity, String your_app_name){
+
+        //-----------------------views init------------------------
         default_rating_msg = dialog.findViewById(R.id.default_rating_msg);
         default_not_now = dialog.findViewById(R.id.default_not_now);
+        ratingBar = dialog.findViewById(R.id.default_rate_bar);
 
+
+        //----------------------------------set view data------------------------
         default_rating_msg.setText("Are you enjoying "+your_app_name+" ? Please rate us");
+
+        if (MyPref.getInt(Constants.COUNTS_FOR_DIALOG_OPEN,0)==3){
+            showRatingDialog();
+        }
 
         default_not_now.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,10 +83,82 @@ public class RatingDialog {
             }
         });
 
-        dialog.show();
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if (rating<4){
+                    closeRatingDialog();
+                    Toast.makeText(context, "Thanks for your feedback",Toast.LENGTH_SHORT).show();
+                }else {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            closeRatingDialog();
+                            openProceedDialog(context,activity);
+                        }
+                    },500);
+                }
+            }
+        });
+
+        if (!MyPref.getBoolean(Constants.IS_JOB_FINISHED,true)){
+            if (MyPref.getInt(Constants.COUNTS_FOR_DIALOG_OPEN,0)==3){
+                dialog.show();
+            }
+        }
     }
 
     private void closeRatingDialog(){
         dialog.dismiss();
+        MyPref.putInt(Constants.COUNTS_FOR_DIALOG_OPEN,0);
+    }
+
+    private void showRatingDialog(){
+        dialog.show();
+        MyPref.putInt(Constants.COUNTS_FOR_DIALOG_OPEN,0);
+    }
+
+    private void openProceedDialog(final Context context, Activity activity){
+        // Build an AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        LayoutInflater inflater = activity.getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.custom_proceed_dialog, null);
+        builder.setView(dialogView);
+
+        // Get the custom alert dialog view widgets reference
+        TextView btn_positive = dialogView.findViewById(R.id.proceed_positive_btn);
+        TextView btn_negative = dialogView.findViewById(R.id.proceed_neutral_btn);
+
+        // Create the alert dialog
+        final AlertDialog alertDialog = builder.create();
+
+        // Set positive/yes button click listener
+        btn_positive.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Dismiss the alert dialog
+                alertDialog.dismiss();
+                Uri uri = Uri.parse("market://details?id=" + context.getApplicationContext().getPackageName());
+                Intent myAppLinkToMarket = new Intent(Intent.ACTION_VIEW, uri);
+                try {
+                    context.startActivity(myAppLinkToMarket);
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(context.getApplicationContext(), " unable to find market app", Toast.LENGTH_LONG).show();
+                }
+                alertDialog.dismiss();
+                MyPref.putBoolean(Constants.IS_JOB_FINISHED, true);
+                MyPref.putInt(Constants.COUNTS_FOR_DIALOG_OPEN, 0);
+            }
+        });
+
+        btn_negative.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+                MyPref.putInt(Constants.COUNTS_FOR_DIALOG_OPEN, 0);
+            }
+        });
+        // Display the custom alert dialog on interface
+        alertDialog.show();
     }
 }
---
+++ ratemyapp/src/main/java/io/tnine/ratemyapp/utils/Constants.java
@@ -0,0 +1,7 @@
+package io.tnine.ratemyapp.utils;
+
+public class Constants {
+    public static String IS_DIALOG_INITIALIZED = "is_dialog_initialized";
+    public static String COUNTS_FOR_DIALOG_OPEN = "counts_for_dialog_open";
+    public static String IS_JOB_FINISHED = "is_job_finished";
+}
